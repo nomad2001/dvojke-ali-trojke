7d3d88b180751d5cea1f83c104531ad1053b674f
Matija Kocbek <mkocbek1@gmail.com>
2021-10-24 20:41:59 +0200

    Dodano razmejevanje med tekmami

--- orodja.py
+++ orodja.py
@@ -32,6 +32,7 @@ def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False):
             count = count + 1
             pripravi_imenik(ime_datoteke)
             with open(ime_datoteke, 'a', encoding='utf-8') as datoteka:
+                datoteka.write("tekma\n")
                 datoteka.write(r.text)
                 print('shranjeno!')
                 print(count)
